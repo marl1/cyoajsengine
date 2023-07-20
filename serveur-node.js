837959f675577cbc0299dc340ad8716cc2b82eab
marclv <levequemc@gmail.com>
2023-07-20 20:39:03 +0200

    Tentative de lancer le serveur+les tests.

--- serveur-node.js
+++ serveur-node.js
@@ -3,5 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/www/'));
 
-app.listen('3000');
-console.log('working on 3000');
+app.listen(
+    3000, 
+    () => console.log(`Server listening on port 3000.`));
