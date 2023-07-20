6e1a8a1f3f24e47c68ed48fdc4763ff522e42663
marclv <levequemc@gmail.com>
2023-07-21 00:34:40 +0200

    Ajout de test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,14 +10,44 @@ test('doit avoir le bon titre de page', async ({ page }) => {
 
 test("doit afficher le bon titre d'épisode", async ({ page }) => {
   await expect(page.locator('#titre')).toHaveText(`Un village simple`);
-})
+});
+
 
 test("doit afficher 0 euros dans le titre du magasin", async ({ page }) => {
-  await page
-  .getByRole('link')
-  .filter({ hasText: 'Aller dans le magasin.' })
-  .click();
-
+  await page.getByRole('link').filter({ hasText: 'Aller dans le magasin.' }).click();
   await expect(page.locator('#titre')).toHaveText(`Aller au magasin (avec 0 euros).`);
-  
+});
+
+test("doit retourner à la page de départ si on sort du magasin.", async ({ page }) => {
+  await page.getByRole('link').filter({ hasText: 'Aller dans le magasin.' }).click();
+  await expect(page.locator('#titre')).toHaveText(`Aller au magasin (avec 0 euros).`);
+  await page.getByRole('link').filter({ hasText: 'Sortir.' }).click();
+  await expect(page.locator('#titre')).toHaveText(`Un village simple`);
+});
+
+test("doit afficher qu'on a pas assez d'argent si on achète la pioche.", async ({ page }) => {
+  await page.getByRole('link').filter({ hasText: 'Aller dans le magasin.' }).click();
+  await expect(page.locator('#titre')).toHaveText(`Aller au magasin (avec 0 euros).`);
+  await page.getByRole('link').filter({ hasText: 'Acheter pioche (200 euros).' }).click();
+  await expect(page.locator('#texte')).toHaveText(`Vous n'avez pas assez d'argent.`);
+});
+
+test("doit afficher qu'on achète une belle pioche si on a assez d'argent", async ({ page }) => {
+  await page.getByRole('link').filter({ hasText: "Aller à l'agence d'interim." }).click();
+  await page.getByRole('link').filter({ hasText: 'Sortir.' }).click();
+
+  await page.getByRole('link').filter({ hasText: "Aller à l'agence d'interim." }).click();
+  await page.getByRole('link').filter({ hasText: 'Sortir.' }).click();
+
+  await page.getByRole('link').filter({ hasText: "Aller à l'agence d'interim." }).click();
+  await page.getByRole('link').filter({ hasText: 'Sortir.' }).click();
+
+  await page.getByRole('link').filter({ hasText: "Aller à l'agence d'interim." }).click();
+  await page.getByRole('link').filter({ hasText: 'Sortir.' }).click();
+
+  await page.getByRole('link').filter({ hasText: 'Aller dans le magasin.' }).click();
+  await expect(page.locator('#titre')).toHaveText(`Aller au magasin (avec 200 euros).`);
+
+  await page.getByRole('link').filter({ hasText: 'Acheter pioche (200 euros).' }).click();
+  await expect(page.locator('#texte')).toHaveText(/une belle pioche/);
 });
