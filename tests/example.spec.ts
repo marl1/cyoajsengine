79382a361a7b61d443306f8c31e282786500b825
marclv <levequemc@gmail.com>
2023-07-20 20:41:31 +0200

    Passage de 2 tests OK !

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://127.0.0.1:3000/jeux/1_jeu_exemple/index.htm');
+});
 
-  // Expect a title "to contain" a substring.
+test('doit avoir le bon titre de page', async ({ page }) => {
   await expect(page).toHaveTitle(/Jeu exemple/);
+});
+
+test("doit afficher le bon titre d'épisode", async ({ page }) => {
+  //await page.isVisible("text='village'")
+  //await page.innerText('#titre');
+  
+
+  const inputElement = page.locator('#titre');
+  const minLength = await expect(inputElement).toHaveText(`Un village simple`);
+
 });
